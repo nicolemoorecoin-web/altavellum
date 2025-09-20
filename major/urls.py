91a1246075630ae1275682ff57059e8f3f961834
nicolemoorecoin-web <nicolemoorecoin@gmail.com>
2025-09-20 06:09:36 -0700

    Update urls.py

--- major/urls.py
+++ major/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from .views import OverviewView, HomeView, AboutView, contact, FaqView # ServiceView # PlanView, ServicesView, RoleView, ContactView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('', HomeView.as_view(), name='home'),
   #  path('<str:ref_code>/', Home_View, name='home'),
     #path('', HomeView.as_view(), name='home'),
